57b871a9e48bea1868772a5f027b9c7cde11e1d4
LeDarkPampa <144164398+LeDarkPampa@users.noreply.github.com>
2023-09-05 22:35:06 +0200

    aaaaaa
    aaaaaaaaaaaaaaaa

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/demo-deplo'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));});
 app.listen(process.env.PORT || 8080);
